3b2256c4ac556b3ef5f6ea3055007749a358c370
Michael Mukiibi <mikemukiibi2005@gmail.com>
2025-01-23 10:54:18 +0300

    added search club button on home, fixed search by name and domain

---
+++ src/routes/SearchButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { Search } from "lucide-svelte";
+</script>
+
+<div class="mx-auto flex flex-row space-x-2">
+    
+    <Button href="/cb/" class="button text-center mx-auto">
+        <Search />
+        Find your club
+    </Button>
+</div>
