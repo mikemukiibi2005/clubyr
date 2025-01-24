77f6e430d97a1c3fecb5d467c77219579c93af6c
Michael Mukiibi <mikemukiibi2005@gmail.com>
2025-01-24 18:48:16 +0300

    added na

---
+++ src/routes/Menu.svelte
@@ -0,0 +1,39 @@
+	
+<script lang="ts">
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+    import { Settings, UserPlus } from "lucide-svelte";
+</script>
+   
+<DropdownMenu.Root>
+    <DropdownMenu.Trigger>
+        <Settings />
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+        <DropdownMenu.Group>
+        <DropdownMenu.GroupHeading>My actions</DropdownMenu.GroupHeading>
+        <DropdownMenu.Separator />
+        <DropdownMenu.Item>
+            <a href="/signup" class="underline">
+                <p>Sign up</p>
+            </a>
+        </DropdownMenu.Item>
+        <DropdownMenu.Item>
+            <a href="/signin" class="underline">
+                <p>Sign in</p>
+            </a>
+        </DropdownMenu.Item>
+        </DropdownMenu.Group>
+        <DropdownMenu.Group>
+            <DropdownMenu.GroupHeading>Club actions</DropdownMenu.GroupHeading>
+            <DropdownMenu.Separator />
+            <DropdownMenu.Item>
+                <a href="/cb" class="underline">Search</a>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item>
+                <a href="/onboarding" class="underline">Create</a>
+            </DropdownMenu.Item>
+        </DropdownMenu.Group>
+    </DropdownMenu.Content>
+</DropdownMenu.Root>
+  
+  
--- src/routes/signin/SigninForm.svelte
+++ src/routes/signin/SigninForm.svelte
@@ -7,13 +7,13 @@
 
 </script>
 <div class="">
-    <form method="post" action="?/signin" class="container mx-auto flex flex-col justify-center space-y-5">
+    <form method="post" action="?/signin" class="flex flex-col justify-center space-y-5">
         <label class="space-y-2">
             <span class="flex flex-row space-x-2 px-3">
                 <Mail />
                 <p class="font-light">Email</p>
             </span>
-            <input name="email" class="w-full border outline-none pl-3 py-1 font-light rounded-lg" />
+            <input placeholder="user@icloud.com" name="email" class="w-full border outline-none pl-3 py-2 font-light rounded-md" />
             
         </label>
         <label class="space-y-2">
@@ -21,7 +21,7 @@
                 <Key />
                 <p class="font-light">Password</p>
             </span>
-            <input name="password" type="password" class="w-full border outline-none pl-3 py-1 rounded-lg" />
+            <input placeholder="*******" name="password" type="password" class="w-full border outline-none pl-3 py-2 rounded-md" />
         </label>
         <Button type="submit">
             <UserPlus />
