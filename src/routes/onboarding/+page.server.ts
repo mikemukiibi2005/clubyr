9b8e918eea74fa545640f43f9af0e54bb75eb5ff
Michael Mukiibi <mikemukiibi2005@gmail.com>
2025-01-24 17:15:24 +0300

    added upload club cover

--- src/routes/onboarding/+page.server.ts
+++ src/routes/onboarding/+page.server.ts
@@ -4,17 +4,16 @@ import type { Actions } from "./$types";
 export const actions: Actions = {
     create: async ({request}) => {
         // Get data
-        const data = await request.formData()
+        const data = await request.formData();
 
-        let club_name = data.get("club")?.toString()
-        let club_domain = data.get("domain")?.toString()
+        // let club_name = data.get("name")?.toString();
+        // let club_domain = data.get("domain")?.toString();
+        // let club_about = data.get("about")?.toString();
+        // let club_cover = data.get("cover");
 
         // Save
-        const club_record = await pb.collection("clubs").create({
-            name: club_name,
-            domain: club_domain,
-        })
+        const club_record = await pb.collection("clubs").create(data);
 
-        return {success: true, id: club_record.id, domain: club_record.domain}
+        return {success: true, id: club_record.id, domain: club_record.domain};
     }
 };
