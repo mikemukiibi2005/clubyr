902903ea9c8372acfbfbfddcc749541c59c733be
Michael Mukiibi <mikemukiibi2005@gmail.com>
2025-02-13 13:38:33 +0300

    refactor: reponsive club search and profile pages

--- src/routes/cb/[domain]/Domain.svelte
+++ src/routes/cb/[domain]/Domain.svelte
@@ -3,7 +3,7 @@
     let full_domain = $derived.by(() => `https://${domain}.clubyr.com`)
 </script>
 
-<div class="space-y-5">
+<div class="md:w-1/2 mx-auto space-y-5">
     <span class="flex flex-row space-x-2">
         <p class="font-light">Website</p>
     </span>
