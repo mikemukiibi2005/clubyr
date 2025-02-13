e11e17b267147e96ed1e9a4fdb7e981cbf2d2876
Michael Mukiibi <mikemukiibi2005@gmail.com>
2025-02-13 13:38:33 +0300

    refactor: reponsive club search and profile pages

--- src/routes/cb/+page.svelte
+++ src/routes/cb/+page.svelte
@@ -7,13 +7,13 @@
     let { form }: { form: ActionData } = $props();
 </script>
 
-<div class="">
+<div class="md:w-1/2 mx-auto">
     <form use:enhance method="post" action="?/search" class="flex flex-col py-5 space-y-3">
         <label class="w-full">
             <input type="text" placeholder="Search by name or domain..." name="club" id="" class="w-full outline-none border rounded-md pl-2 py-2 font-light text-sm text-center" />
             {#if form?.success}
                 {#each form?.results as {name, domain}}
-                    <div class="my-1">
+                    <div class="my-2">
                         <a href={`/cb/${domain}`}>
                             <p class="p-2 rounded-md hover:bg-slate-200 text-sm font-light underline">{name}</p>
                         </a>
